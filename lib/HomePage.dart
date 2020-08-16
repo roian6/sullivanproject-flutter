9b9daed762abea2e0ef6211156df7f0f2774cc8f
roian6 <roian6@naver.com>
2020-08-16 15:57:10 +0900

    finish app

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,13 +1,12 @@
-import 'dart:async';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
+import 'package:flutter/services.dart';
+import 'package:flutterapp/ChatPage.dart';
 import 'package:flutterapp/HistoryPage.dart';
-import 'package:flutterapp/SoundPager.dart';
-import 'package:noise_meter/noise_meter.dart';
-
-import 'SoundData.dart';
-import 'SqliteUtil.dart';
+import 'package:flutterapp/SoundPage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
+import 'package:shared_preferences/shared_preferences.dart';
 
 /// 첫 페이지
 /// 소음을 측정하는 화면입니다.
@@ -16,48 +15,79 @@ class HomePage extends StatefulWidget {
   State<StatefulWidget> createState() => HomePageState();
 }
 
-class HomePageState extends State<StatefulWidget> {
-  NoiseMeter noiseMeter = new NoiseMeter();
-  StreamSubscription<NoiseReading> subscription;
-  SoundPager clip = new SoundPager();
+class HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+  final List<Widget> tabs = [SoundPage(), ChatPage()];
+  String title;
 
-  final int limitDecibel = 100;
-  int currentDecibel = 0;
+  void onNavTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  void logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Firestore.instance
+        .collection("apartment")
+        .document(prefs.getString("id"))
+        .updateData({"login": false});
+
+    await prefs.setString("id", null);
+    SystemNavigator.pop();
+  }
+
+  void getCurrentId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      title = prefs.getString("id") + "호";
+    });
+  }
+
+  Future scan() async {
+    String code = await scanner.scan();
+    code = code.replaceAll("sullivan: ", "");
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String me = prefs.getString("id");
+    Firestore.instance.collection("apartment").document(me).updateData({
+      "nb": FieldValue.arrayUnion([code])
+    }).then((value) {
+      Firestore.instance.collection("apartment").document(code).updateData({
+        "nb": FieldValue.arrayUnion([me])
+      }).then((value) => toast("성공적으로 추가했습니다!"));
+    });
+  }
+
+  void toast(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
 
   @override
   void initState() {
     super.initState();
-    this.subscription = noiseMeter.noiseStream.listen(this.onData);
-  }
-
-  @override
-  void onData(NoiseReading sound) {
-    setState(() {
-      var decibel = sound.maxDecibel.toInt();
-      this.currentDecibel = decibel;
-      this.clip.addSound(decibel);
-    });
-  }
-
-  bool isSafeDecibel() {
-    return currentDecibel < limitDecibel;
-  }
-
-  // 0.0 ~ 1.0 사이의 숫자가 반환됩니다.
-  double currentDecibelPercent() {
-    if (currentDecibel > limitDecibel) {
-      return 1;
-    } else {
-      return currentDecibel / limitDecibel;
-    }
+    title = "";
+    getCurrentId();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("소음 측정기"), actions: [
+      appBar:
+          AppBar(title: Text(title), backgroundColor: Colors.blue, actions: [
         IconButton(
-          icon: new Icon(Icons.search),
+          icon: new Icon(Icons.exit_to_app),
+          highlightColor: Colors.white,
+          onPressed: () => logout(),
+        ),
+        IconButton(
+          icon: new Icon(Icons.history),
           highlightColor: Colors.white,
           onPressed: () {
             Navigator.of(context).push(
@@ -67,53 +97,23 @@ class HomePageState extends State<StatefulWidget> {
             );
           },
         ),
+        IconButton(
+          icon: new Icon(Icons.center_focus_weak),
+          highlightColor: Colors.white,
+          onPressed: () => scan(),
+        ),
       ]),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 16, 0, 0),
-            child: FAProgressBar(
-              direction: Axis.vertical,
-              currentValue: currentDecibel,
-              maxValue: limitDecibel,
-              borderRadius: 0,
-              verticalDirection: VerticalDirection.up,
-              progressColor: isSafeDecibel() ? Colors.blue : Colors.red,
-            ),
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: <Widget>[
-              SizedBox(
-                width: 150,
-                height: 150,
-                child: CircularProgressIndicator(
-                  value: currentDecibelPercent(),
-                  strokeWidth: 12,
-                  valueColor: AlwaysStoppedAnimation(
-                      isSafeDecibel() ? Colors.blue : Colors.red),
-                ),
-              ),
-              Text("$currentDecibel db", style: TextStyle(fontSize: 30)),
-            ],
-          )
+      body: tabs[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: onNavTap,
+        currentIndex: currentIndex,
+        items: [
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.show_chart), title: Text("소음 측정")),
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.tag_faces), title: Text("이모티콘"))
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // recording 중일경우, 녹음을 종료하고 soundData 정보를 저장한다.
-          if (clip.isRecoding) {
-            SoundData data = clip.finishRecoding();
-            print("${data.date} : ${data.averageDecibel} / ${data.maxDecibel}");
-            SqliteUtil.onAdd(data);
-          }
-          // 아닐 경우 녹음을 시작한다.
-          else {
-            clip.startRecoding();
-          }
-        },
-        child: clip.isRecoding ? Icon(Icons.pause) : Icon(Icons.play_arrow),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp/HomePage.dart';
-import 'package:flutterapp/HistoryPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'SetupPage.dart';
 
 /// main 함수
 /// Flutter 앱의 첫 시작은 여기부터입니다.
@@ -10,19 +13,48 @@ void main() {
 
 /// 처음에 실행시켜줄 화면을 보여줍니다.
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  var title = '설리번 프로젝트';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "층간소음 앱",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: new Splash());
+  }
+}
+
+class Splash extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => SplashState();
+}
+
+class SplashState extends State<Splash> with AfterLayoutMixin<Splash> {
+  Future checkLogin() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLogin = false;
+    if (prefs.getString("id") != null) {
+      if (prefs.getString("id").isNotEmpty) isLogin = true;
+    }
+
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => isLogin ? HomePage() : SetupPage()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: CircularProgressIndicator(),
       ),
-      home: HomePage(),
-//      home: HistoryPage(title: "층간소음 녹음내역"),
     );
   }
+
+  @override
+  void afterFirstLayout(BuildContext context) => checkLogin();
 }
